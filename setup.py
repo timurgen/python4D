41752688c51857c130aa690aafb60ee34af111d7
marcianobarros20 <marcianobarros20@hotmail.com>
2018-03-01 20:38:02 +0000

    setup update

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from setuptools.command.install import install
 from distutils.ccompiler import new_compiler
 import os
 
-
 def get_ext_modules():
     import p4d
     return [p4d.ffi.verifier.get_extension()]
@@ -36,13 +35,13 @@ setup(
         "build": CFFIBuild,
         "install": CFFIInstall,
     },
-    author="Israel Brewster",
+    author="Marciano Barros",
     author_email="marcianobarros20@hotmail.com",
     url="https://github.com/marcianobarros20/p4d",
-
     description="Python DBI module for the 4D database",
-    long_description="This module provides a Python Database API v2.0 compliant driver for the 4D (4th Dimension, http://www.4d.com ) database. Based off of C library code provided by 4th Dimension and implemented using CFFI",
-
+    long_description="This module provides a Python Database API v2.0 "
+                     "compliant driver for the 4D (4th Dimension, http://www.4d.com ) database. "
+                     "Based off of C library code provided by 4th Dimension and implemented using CFFI",
     license='BSD',
     classifiers=['Development Status :: 5 - Production/Stable',
                  'License :: OSI Approved :: BSD License',
