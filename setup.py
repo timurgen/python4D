b514873f3c1db27f24d2f82fe8a6293e629a6a02
marcianobarros20 <marcianobarros20@hotmail.com>
2018-03-02 00:11:37 +0000

    .gitignore update

--- setup.py
+++ setup.py
@@ -39,15 +39,13 @@ setup(
     author_email="marcianobarros20@hotmail.com",
     url="https://github.com/marcianobarros20/p4d",
     description="Python DBI module for the 4D database",
-    long_description="This module provides a Python Database API v2.0 "
-                     "compliant driver for the 4D (4th Dimension, http://www.4d.com ) database. "
-                     "Based off of C library code provided by 4th Dimension and implemented using CFFI",
+    long_description="This module provides a Python Database API v2.0 compliant driver for the 4D (4th Dimension, http://www.4d.com ) database. Based off of C library code provided by 4th Dimension and implemented using CFFI",
     license='BSD',
     classifiers=['Development Status :: 5 - Production/Stable',
                  'License :: OSI Approved :: BSD License',
                  'Intended Audience :: Developers',
                  'Topic :: Database',
-		 'Programming Language :: Python :: 3',
-		 'Programming Language :: Python :: 2'],
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 2'],
     keywords='datababase drivers DBI 4d'
 )
